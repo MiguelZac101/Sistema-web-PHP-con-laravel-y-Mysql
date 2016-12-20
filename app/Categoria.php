4ab8af568e03c610d43e0c16bbf469f19064a5ea
Miguel Quispe <miguel.quispe@fractal.com.pe>
2016-12-20 15:20:39 -0500

    add categoria

---
+++ app/Categoria.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = "categoria";
+    protected $primaryKey = "idcategoria";
+    public $timestamps = false;
+    protected $fillable = [
+    	'nombre',
+    	'descripcion',
+    	'condicion'
+    ];
+    protected $guarded = [
+
+    ];
+}
