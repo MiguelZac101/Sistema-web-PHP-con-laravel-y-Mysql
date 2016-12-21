d74ea1b4616136d02f0fb1b40673309f11d71fda
Miguel Quispe <miguel.quispe@fractal.com.pe>
2016-12-21 15:14:42 -0500

    CategoriaController

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Categoria;
+use Illuminate\Support\Facades\Redirect;
+use App\Http\Requests\CategoriaFormRequest;
+use DB;
+
+class CategoriaController extends Controller
+{
+    public function __construct() {
+        
+    }
+    public function index(Request $request){
+        if($request){
+            $query = trim($request->get('searchText'));
+            $categoria = DB::table('categoria')->where('nombre','LIKE','%'.$query.'%')
+            ->where('condicion','=','1')
+            ->ordeBy('idcategoria','desc')
+            ->paginate(7);
+            
+            $data = ["categorias"=>$categorias,"searchText"=>$query];
+            
+            return view('almacen.categoria.index',$data);
+        }
+    }
+    public function create(){
+        return view("almacen.categoria.create");
+    }
+    public function store(CategoriaFormRequest $request){
+        $categoria = new Categoria;
+        $categoria->nombre = $request->get('nombre');
+        $categoria->descripcion = $request->get('descripcion');
+        $categoria->condicion = '1';
+        $categoria->save();
+        return Redirect::to('almacen/categoria');
+        
+    }
+    public function show($id){
+        $data = ["categoria"=>Categoria::findOrFail($id)];
+        return view('almacen.categoria.show',$data);
+    }
+    public function edit($id){
+        $data = ["categoria"=>Categoria::findOrFail($id)];
+        return view('almacen.categoria.edit',$data);
+    }
+    public function update(CategoriaFormRequest $request,$id){
+        $categoria = Categoria::findOrFail($id);
+        $categoria->nombre = $request->get('nombre');
+        $categoria->descripcion = $request->get('descripcion');
+        $categoria->update();
+        return Redirect::to('almacen/categoria');
+    }
+    public function destroy($id){
+        $categoria = Categoria::findOrFail($id);
+        $categoria->condicion = "0";
+        $categoria->update();
+        return Redirect::to('almacen/categoria');
+    }
+}
